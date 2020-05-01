fefb89fe4fcac4ca49fa60b1321c83837e354136
John Celoria <jceloria@gmail.com>
2020-05-01 15:17:47 -0400

    persistent redis storage

--- README.md
+++ README.md
@@ -16,6 +16,10 @@ Download or clone the repo, then open up terminal and cd into the app's root.
 npm install
 ```
 
+Most recent update added Redis support, to store configs from user. The user also can set their own OOO from Glip, by talking to themself.
+
+Install Redis on your server, and this app expects that it will use all default settings from a new local Redis install.
+
 ## Setup
 Make a copy of the `env_example` file, and rename it `.env`. Then edit the fields in it. 
 
@@ -24,5 +28,21 @@ With all that set up, you can kick it off from command line using `node .`
 
 If you want to get fancy, you can use something like [PM2](https://pm2.keymetrics.io) to manage keeping the app alive for you. 
 
-## Autho
+## Usage
+
+To talk to your bot directly, you're actually going to talk to yourself in Glip. You can select your icon at the top right, and send yourself a message from there. 
+
+Commands: 
+
+1. ping
+    * Responds with pong, just to let you know the service is up.
+2. get ooo
+    * Responds back with any current ooo settings stored in redis
+3. new ooo
+    * Lets you set a new out of office message using the following format
+    * new ooo - 05/01/2020 - 05/05/2020 - I will be out of office until the 6th. Reach out to so and so
+4. test
+    * Responds to you directly as though you'd been @ mentioned
+
+## Author
 John Celoria jceloria@gmail.com
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const SDK = require('@ringcentral/sdk').SDK
 const Subscriptions = require('@ringcentral/subscriptions').Subscriptions;
 const { LocalStorage } = require('node-localstorage')
-const dotenv = require('dotenv')
+require('dotenv').config()
 const moment = require('moment')
 
-dotenv.config()
 const localstorage = new LocalStorage('./auth')
 
 const startDate = moment(process.env.START_DATE).startOf('day')
 const endDate = moment(process.env.END_DATE).endOf('day')
+
+const { setUserData, getUserData, openAllConnections } = require('./lib/redis')
 
 const rcsdk = new SDK({
     server: process.env.RINGCENTRAL_PLATFORM_ADDRESS,
@@ -33,6 +34,7 @@ rcsdk
     })
     .then(() => {
         console.log("Logged in successfully")
+        openAllConnections()
         main()
     })
     .catch(e => {
@@ -59,28 +61,51 @@ async function main() {
     let subscription = subscriptions.createSubscription();
 
     subscription.on(subscription.events.notification, async msg => {
-        if (await wasIMentioned(msg.body) === true && moment().isBetween(startDate, endDate) == true) {
-            rcsdk
-                .platform()
-                .post(`/restapi/v1.0/glip/posts`, {
-                    text: process.env.RESPONSE_MESSAGE,
-                    personIds: [msg.body.creatorId]
-                })
-                .catch(e => {
-                    console.error("Issue responding to @mention: ", e.message)
-                })
+
+        if (msg.body.creatorId == userExtensionId && msg.body.text && /^ping/.test(msg.body.text)) {
+            pong(msg.body.creatorId)
+            return
         }
 
-        if (msg.body.creatorId == userExtensionId && msg.body.test && msg.body.text.includes("ping")) {
-            rcsdk
-                .platform()
-                .post(`/restapi/v1.0/glip/posts`, {
-                    text: "pong",
-                    personIds: [msg.body.creatorId]
-                })
-                .catch(e => {
-                    console.error("Issue responding to @mention: ", e.message)
-                })
+        if (msg.body.creatorId == userExtensionId && msg.body.text && /^new ooo/.test(msg.body.text)) {
+            console.log("New ooo: ", msg.body)
+            newOoo(msg.body.creatorId, msg.body.text)
+            return
+        }
+
+        if (msg.body.creatorId == userExtensionId && msg.body.text && /^get ooo/.test(msg.body.text)) {
+            console.log("Info on ooo: ", msg.body)
+            info(msg.body.creatorId)
+            return
+        }
+
+        if (msg.body.creatorId == userExtensionId && msg.body.text && /^test ooo/.test(msg.body.text)) {
+            console.log("Info on ooo: ", msg.body)
+
+            let oooInfo = await getUserData(userExtensionId).catch(e => {
+                console.error("Issue getting user info: ", e)
+                return undefined
+            })
+
+            autoResponse(userExtensionId, msg.body.creatorId, oooInfo)
+            return
+        }
+
+        let wasMentioned = await wasIMentioned(msg.body)
+
+        if (!wasIMentioned) { return }
+
+        let oooInfo = await getUserData(userExtensionId).catch(e => {
+            console.error("Issue getting user info: ", e)
+            return undefined
+        })
+
+        if (!oooInfo) { return }
+
+        let isBetween = moment().isBetween(oooInfo.startDate, oooInfo.endDate)
+
+        if (wasMentioned === true && isBetween == true) {
+            autoResponse(userExtensionId, msg.body.creatorId, oooInfo)
         }
 
     });
@@ -118,15 +143,124 @@ async function wasIMentioned(msg) {
 
 }
 
-// async function cleanSubs(subsList) {
+/**
+ * Response pong to an activity test from user
+ * 
+ * @param string extensionId 
+ */
+async function pong(extensionId) {
+    rcsdk
+        .platform()
+        .post(`/restapi/v1.0/glip/posts`, {
+            text: "pong",
+            personIds: [extensionId]
+        })
+        .catch(e => {
+            console.error("Issue responding to @mention: ", e.message)
+        })
+}
 
-//     subsList.forEach(sub => {
-//         rcsdk
-//             .platform()
-//             .delete(`/restapi/v1.0/subscription/${sub.id}`)
-//             .catch(e => {
-//                 console.error("Issue deleing sub: ", e.message)
-//             })
-//     })
+/**
+ * Responds to a user request about info on their current OOO setting
+ * 
+ * @param string extensionId 
+ */
+async function info(extensionId) {
 
-// }
+    let oooInfo = await getUserData(extensionId.toString())
+
+    let responseText = []
+
+    Object.keys(oooInfo).forEach(key => {
+        responseText.push(`${key}: ${oooInfo[key]}`)
+    })
+
+    rcsdk
+        .platform()
+        .post(`/restapi/v1.0/glip/posts`, {
+            text: responseText.join(", "),
+            personIds: [extensionId]
+        })
+        .catch(e => {
+            console.error("Issue responding to @mention: ", e.message)
+        })
+}
+
+/**
+ * Response pong to an activity test from user
+ * 
+ * @param string extensionId 
+ * @param string Message test to parse for valid start and end dates, and message to respond with
+ */
+async function newOoo(extensionId, message) {
+
+    message = message.replace('newooo', '')
+
+    let split = message.split('-')
+
+    let errorMessage = []
+
+    let oooInfo = {
+        startDate: moment(split[1]).toISOString(),
+        endDate: moment(split[2]).toISOString(),
+        message: split[3]
+    }
+
+    if (!moment(oooInfo.startDate).isValid()) {
+        errorMessage.push("Start date is not valid. Use MM/DD/YYYY")
+    }
+
+    if (!moment(oooInfo.endDate).isValid()) {
+        errorMessage.push("End date is not valid. Use MM/DD/YYYY")
+    }
+
+    if (!oooInfo.message) {
+        errorMessage.push("Something went wrong with the Out of Office message text")
+    }
+
+    if (errorMessage.length > 0) {
+        errorMessage.push("\n\nMessage content should look like:\nnew ooo - 04/01/2020 - 04/05/2020 - This is the out of office response message")
+    }
+
+    console.log(oooInfo)
+
+    console.log(errorMessage)
+
+    if (moment(oooInfo).isBefore(moment())) {
+        errorMessage.push("End date must be after today's date")
+    }
+
+    let expiresIn = moment(oooInfo.endDate) - moment()
+
+    setUserData(extensionId, oooInfo, expiresIn)
+        .catch(e => {
+            errorMessage.push(e)
+        })
+
+    rcsdk
+        .platform()
+        .post(`/restapi/v1.0/glip/posts`, {
+            text: errorMessage.length > 0 ? errorMessage.join(', ') : "Set successfully",
+            personIds: [extensionId]
+        })
+        .catch(e => {
+            console.error("Issue responding to @mention: ", e.message)
+        })
+}
+
+async function autoResponse(extensionId, messageRecipient, ooInfo) {
+
+    let oooInfo = await getUserData(extensionId)
+
+    let respoonseMessage = `Automated Response: ${oooInfo.message}`
+
+    rcsdk
+        .platform()
+        .post(`/restapi/v1.0/glip/posts`, {
+            text: respoonseMessage,
+            personIds: [messageRecipient]
+        })
+        .catch(e => {
+            console.error("Issue responding to @mention: ", e.message)
+        })
+}
